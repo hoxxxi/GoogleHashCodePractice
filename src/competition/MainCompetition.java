f46a292392268cea3ed34759e0a1203575d97ebd
Yordan <hoxxxii@gmail.com>
2016-02-11 19:44:05 +0000

    Initial Infrastructure, including Warehouse, Product and Orders

--- src/competition/MainCompetition.java
+++ src/competition/MainCompetition.java
@@ -1,5 +1,57 @@
 package competition;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Stack;
+
 public class MainCompetition {
 
+	public static void main(String[]args) throws IOException
+	{
+		//Take input
+		BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\User\\workspace\\GoogleHashCode\\src\\competition\\input.txt"));
+		
+		String []landscapeDefinition = in.readLine().split(" ");//{"100", "100", "3", "50", "500"}; // grid h, grid w, n of drones, deadline, drone payload
+		
+		
+		int numberOfProductTypes = Integer.parseInt(in.readLine());
+		ArrayList<Product> productTypes = new ArrayList<Product>();
+		String []individualProductWeight = in.readLine().split(" ");
+		for(int asd = 0; asd<numberOfProductTypes; asd++)
+		{
+			productTypes.add(new Product(asd, Integer.parseInt(individualProductWeight[asd])));
+		}
+	
+		
+		
+		int numberOfWarehouses = Integer.parseInt(in.readLine());
+		ArrayList<Warehouse> warehouseList = new ArrayList<Warehouse>();
+		for(int currw = 0;currw<numberOfWarehouses;currw++)
+		{
+			String []warehouseCoordinate1 = in.readLine().split(" ");
+			String []quanttyOfEachProductatWarehouse1 = in.readLine().split(" ");
+		
+			HashMap<Product, Integer> inventory = new HashMap<>();
+			
+			for(int bah = 0; bah<numberOfProductTypes; bah++)
+			{
+				inventory.put(productTypes.get(bah), Integer.parseInt(quanttyOfEachProductatWarehouse1[bah]));
+			}
+			warehouseList.add(new Warehouse(Integer.parseInt(warehouseCoordinate1[0]),Integer.parseInt(warehouseCoordinate1[0]),inventory));
+			
+		}		
+		int numberOfOrders = Integer.parseInt(in.readLine());
+		Stack<Order> orderList = new Stack<Order>();
+		for(int i =0;i<numberOfOrders;i++)
+		{
+			String []orderCoordinates1 = in.readLine().split(" ");
+			int numberOFItems1 = Integer.parseInt(in.readLine());
+			String []productTypes1 = in.readLine().split(" "); //Change to dictionary
+			orderList.add(new Order(Integer.parseInt(orderCoordinates1[0]), Integer.parseInt(orderCoordinates1[1]), productTypes1));
+		}
+	}
 }
---
+++ src/competition/Order.java
@@ -0,0 +1,13 @@
+package competition;
+
+public class Order {
+	int x;
+	int y;
+	String []itemIDs;
+	public Order(int xIn, int yIn, String[]itemsIDsIn)
+	{
+		this.x=xIn;
+		this.y=yIn;
+		this.itemIDs=itemsIDsIn;
+	}
+}
