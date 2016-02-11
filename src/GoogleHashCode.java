ad832485a3a7568c2484ff461742b58054dc6996
Stasiek Abyszkin <stasiek@Stasieks-MacBook-Pro.local>
2016-02-11 12:57:58 +0000

    Implemented printing out to file and checking input with output.

--- src/GoogleHashCode.java
+++ src/GoogleHashCode.java
@@ -1,8 +1,10 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 public class GoogleHashCode {
@@ -10,17 +12,16 @@ public class GoogleHashCode {
 	public static void main(String[]args) throws IOException
 	{
 //		BufferedReader in = new BufferedReader(new FileReader("input.txt"));
-		BufferedReader in = new BufferedReader(new FileReader("logo.in"));
+//		BufferedReader in = new BufferedReader(new FileReader("logo.in"));
+//		BufferedReader in = new BufferedReader(new FileReader("right_angle.in"));
+		BufferedReader in = new BufferedReader(new FileReader("learn_and_teach.in"));
+		
 		String row_col[] = in.readLine().split(" ");
 
 		int rows = Integer.parseInt(row_col[0]);
 		int cols = Integer.parseInt(row_col[1]);
 		
 		String board[][] = new String[rows][cols];
-		
-		String testBoard[][] = new String[rows][cols];
-		for(int i =0;i<testBoard.length;i++)
-			Arrays.fill(testBoard[i], ".");
 		
 		for(int i = 0; i < rows; i++)
 		{
@@ -49,7 +50,7 @@ public class GoogleHashCode {
 		
 		
 		int counter = 0;
-		
+		ArrayList<String> operations = new ArrayList<String>();
 		for(Vector vector: vectors)
 		{
 			int[] start = vector.getStartPosition();
@@ -57,15 +58,33 @@ public class GoogleHashCode {
 			if(r.canDrawVectorOnBoard(vector))
 			{
 				System.out.println("start:(" + start[0] + ", " + start[1] + ") , end:(" + end[0] + ", " +end[1]+ ")");
-				r.writeLine(start[0], start[1], end[0], end[1]);
+				operations.add(r.writeLine(start[0], start[1], end[0], end[1]));
 				counter++;
 			}
 			else 
 				continue;
 		}
+		r.printCanvas();
 		System.out.println("Number of instructions = " + counter);
 		System.out.println("Points earned = "+ (rows*cols - counter));
-		r.printCanvas();
+		System.out.println("Canvas are matched:" + r.compareInputWithOutput());
+		File file = new File("rigthAngleOutput.txt");
+
+        // if file doesnt exists, then create it
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        FileWriter fw = new FileWriter(file.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(fw);
+        
+        bw.write(operations.size() + "\n");
+        for(String str:operations){
+        	bw.write(str + "\n");
+        }
+        
+        bw.close();
+
 	}
 	
 }
--- src/Robot.java
+++ src/Robot.java
@@ -5,165 +5,154 @@ public class Robot {
 	public int counter = 0;
 	String board[][];
 	String canvas[][];
-	
-	public Robot(String boardIn[][])
-	{
+
+	public Robot(String boardIn[][]) {
 		this.counter = 0;
 		this.board = boardIn;
-		
+
 		int r = board.length;
 		int c = 0;
-		
-		try{
+
+		try {
 			c = board[0].length;
-		}
-		catch (IndexOutOfBoundsException e)
-		{
+		} catch (IndexOutOfBoundsException e) {
 			System.err.println("Empty input");
 		}
-		
-	
+
 		this.canvas = new String[r][c];
-		for(int i =0;i<canvas.length;i++)
+		for (int i = 0; i < canvas.length; i++)
 			Arrays.fill(canvas[i], ".");
-		
+
 	}
-	public String writeSquare(int row, int col, int size)
-	{
-		size = 2*size +1;
-		
+
+	public String writeSquare(int row, int col, int size) {
+		size = 2 * size + 1;
+
 		int x = 0;
 		int y = 0;
-		
-		x = row - size/2;
-		y = col - size/2;
-		
-		try{
-			for(int i = x; i<size; i++)
-			{
-				for(int k = y; k<size; k++)
-				{
-					this.canvas[i][k]="#";
+
+		x = row - size / 2;
+		y = col - size / 2;
+
+		try {
+			for (int i = x; i < size; i++) {
+				for (int k = y; k < size; k++) {
+					this.canvas[i][k] = "#";
 				}
 			}
 			this.counter++;
-		}
-		catch (IndexOutOfBoundsException e)
-		{
+		} catch (IndexOutOfBoundsException e) {
 			System.err.println("Drawing square outside board boundaries.");
 		}
-		
-		return "Print Square - X: "+x+" Y: " + y + " Size: " + size;
+
+		return "Print Square - X: " + x + " Y: " + y + " Size: " + size;
 	}
-	public String writeLine(int r1, int c1, int r2, int c2)
-	{
-		try{
-			if(r1==r2 || c1==c2){
-				for(int i = r1; i<=r2; i++)
-				{
-					for(int k = c1; k<=c2; k++)
-					{
-						this.canvas[i][k]="#";
+
+	public String writeLine(int r1, int c1, int r2, int c2) {
+		try {
+			if (r1 == r2 || c1 == c2) {
+				for (int i = r1; i <= r2; i++) {
+					for (int k = c1; k <= c2; k++) {
+						this.canvas[i][k] = "#";
 					}
 				}
 				this.counter++;
-			}
-			else 
-			{
+			} else {
 				System.err.println("Input does not mach a single line vector.");
 			}
-		}
-		catch (IndexOutOfBoundsException e)
-		{
+		} catch (IndexOutOfBoundsException e) {
 			System.err.println("Vector dimensions outside board boundaries.");
 		}
-		
-		return "Print Line from X: "+ r1 +" Y: " + c1 + " to X: " + r2 + " Y: " + c2;
+
+		return "PAINT_LINE " + r1 + " " + c1 + " " + r2 + " " + c2;
 	}
-	public String eraseCell(int row, int col)
-	{
-		try{
-				this.canvas[row][col]=".";
-				this.counter++;
-		}
-		catch (IndexOutOfBoundsException e)
-		{
+
+	public String eraseCell(int row, int col) {
+		try {
+			this.canvas[row][col] = ".";
+			this.counter++;
+		} catch (IndexOutOfBoundsException e) {
 			System.err.println("Erase cell outside board boundaries.");
 		}
-		
-		return "Erase Cell - X: "+row+" Y: " + col;
+
+		return "Erase Cell - X: " + row + " Y: " + col;
 	}
-	public void printCanvas()
-	{
-		try{
-			for(int i =0 ; i<canvas.length; i++)
-			{
-				for(int k = 0; k<canvas[0].length; k++)
+
+	public void printCanvas() {
+		try {
+			for (int i = 0; i < canvas.length; i++) {
+				for (int k = 0; k < canvas[0].length; k++)
 					System.out.print(canvas[i][k]);
 				System.out.println();
 			}
-		}
-		catch (IndexOutOfBoundsException e)
-		{
+		} catch (IndexOutOfBoundsException e) {
 			System.err.println("Empty canvas.");
-		} 
+		}
 	}
+
+	public boolean compareInputWithOutput() {
+		int mismatched = 0;
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				if (!board[i][j].equals(canvas[i][j]))
+					mismatched++;
+			}
+		}
+		if(mismatched == 0)
+			return true;
+		else{
+			System.err.println("Number of mismatched:" + mismatched);
+			return false;
+		}
+	}
+
 	public void printInput() {
-		for(int i = 0; i < board.length; i++)
-		{
-			for(int k = 0; k < board[0].length; k++)
-			{
+		for (int i = 0; i < board.length; i++) {
+			for (int k = 0; k < board[0].length; k++) {
 				System.out.print(board[i][k]);
 			}
 			System.out.println();
-		}			
+		}
 	}
-	public String calculateCanvas()
-	{
+
+	public String calculateCanvas() {
 		String result = "";
-		try{
-			for(int i = 0;i<canvas.length;i++)
-			{
-				for(int k = 0; k<canvas[0].length;k++)
-				{
-					if(board[i][k].equals("#"))
-					{
-						result += this.writeLine(i, k, i, k)+"\n";
+		try {
+			for (int i = 0; i < canvas.length; i++) {
+				for (int k = 0; k < canvas[0].length; k++) {
+					if (board[i][k].equals("#")) {
+						result += this.writeLine(i, k, i, k) + "\n";
 					}
 				}
 			}
-		}
-		catch (IndexOutOfBoundsException e)
-		{
+		} catch (IndexOutOfBoundsException e) {
 			System.err.println("You fcuked up!");
 		}
 		result = this.counter + "\n" + result;
 		return result;
 	}
 
-	public boolean canDrawVectorOnBoard(Vector v)
-	{
+	public boolean canDrawVectorOnBoard(Vector v) {
 		int[] startPos = v.getStartPosition();
 		int[] endPos = v.getEndPosition();
-		
-		boolean isHorizontal = startPos[1] - endPos[1] == 0;  
-		
-		for (int i = 0; i < v.getLength()+1; i++) {
-			if (isHorizontal){
-				if ( canvas[startPos[0] + i][startPos[1]].equals("#") ){
+
+		boolean isHorizontal = startPos[1] - endPos[1] == 0;
+
+		for (int i = 0; i < v.getLength() + 1; i++) {
+			if (isHorizontal) {
+				if (canvas[startPos[0] + i][startPos[1]].equals("#")) {
 					return false;
 				}
-			}
-			else{
-				if (canvas[startPos[0]][startPos[1] + i].equals("#"))
-				{
+			} else {
+				if (canvas[startPos[0]][startPos[1] + i].equals("#")) {
 					return false;
 				}
 			}
 		}
 		return true;
 	}
-	public int getCounter(){
+
+	public int getCounter() {
 		return counter;
 	}
 
